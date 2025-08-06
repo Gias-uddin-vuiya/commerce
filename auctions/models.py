a2e884c8cad3bf87c8d6ec0f4c2422ca8b51439e
Gias-uddin-vuiya <giasuddinvuiya2022@gmail.com>
2025-08-06 15:41:39 +0600

    add Bids models

--- auctions/models.py
+++ auctions/models.py
@@ -25,3 +25,13 @@ class Watchlist(models.Model):
     
     def __str__(self):
         return f"{self.user.username} - {self.auction.title}"
+
+
+class Bids(models.Model):
+    auction = models.ForeignKey(Auctions, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    bid_amount = models.DecimalField(max_digits=10, decimal_places=2)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.user.username} - {self.auction.title} - ${self.bid_amount}"
